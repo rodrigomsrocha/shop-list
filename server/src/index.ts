159123d5b213a056eba083a10f271fcdf20cd71d
rodrigo <rodrigomtc82@gmail.com>
2023-09-20 16:30:03 -0300

    setup eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
-import { serve } from "@hono/node-server";
-import { Hono } from "hono";
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
 
-const app = new Hono();
-app.get("/", (c) => c.text("Hello Hono!"));
+const app = new Hono()
+app.get('/', (c) => c.text('Hello Hono!'))
 
 serve(app, (address) => {
-  console.log(`Server running on http://localhost:${address.port}`);
-});
+  console.log(`Server running on http://localhost:${address.port}`)
+})
